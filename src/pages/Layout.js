ac794891db6cf22949d36384110bd473dc1d94d5
kjdominick66 <kjdominick66@gmail.com>
2026-01-15 13:13:17 -0500

    Added containers for links changed website title and description.

--- src/pages/Layout.js
+++ src/pages/Layout.js
@@ -3,10 +3,10 @@ import { Outlet, Link } from "react-router-dom";
 function Layout(){
     return(
         <div>
-            <nav>
-                <Link to="/">Home</Link>
-                <Link to="/about">About Me</Link>
-                <Link to="/projects">Projects</Link>
+            <nav className="navbar">
+                <Link to="/"><span>Home</span></Link>
+                <Link to="/about"><span>About Me</span></Link>
+                <Link to="/projects"><span>Projects</span></Link>
             </nav>
             <hr />
             <Outlet />
