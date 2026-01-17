f5f0a8eec1b68096e4fe05efbceadf51f57bf9fc
kjdominick66 <kjdominick66@gmail.com>
2026-01-17 14:47:14 -0500

    Positioning fixes

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,7 +5,9 @@ function Home() {
     return (
         <div className='home-container'>
             <h1>Home Page</h1>
-            <img className='mountainimg' src={OnAMountain} alt="OnAMountain" />
+            <div className='image-container'>
+                <img className='mountainimg' src={OnAMountain} alt="OnAMountain" />
+            </div>
         </div>
     );
 }
