842a9ed74f7b30f21f77e4a2514449e7e60bd49d
kjdominick66 <kjdominick66@gmail.com>
2026-01-17 13:49:00 -0500

    Added image to website!

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,11 @@
 import './Home.css';
+import OnAMountain from '../models/keenanmountain.jpg'
 
 function Home() {
     return (
         <div className='home-container'>
             <h1>Home Page</h1>
+            <img className='mountainimg' src={OnAMountain} alt="OnAMountain" />
         </div>
     );
 }
